cd9ece60cab24679a6af3d0b37319a8615514ec9
Lena Voytek <dvoytek@email.arizona.edu>
2019-12-12 13:45:45 -0700

    Added blow and hole sensor files

---
+++ OcarinaFirmware/src/BlowSensor.h
@@ -0,0 +1,18 @@
+/*  
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ * 
+ * Author: Lena Voytek
+ * 
+ * BlowSensor.h
+ * Contains a class that uses Adafruit's MPRLS pressure sensor library
+ * to determine the incoming ocarina air pressure, and therefore the 
+ * relative output volume.
+ */
+
+#ifndef BLOWSENSOR_H
+#define BLOWSENSOR_H
+
+
+#endif
---
+++ OcarinaFirmware/src/HoleSensor.h
@@ -0,0 +1,20 @@
+/*  
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ * 
+ * Author: Lena Voytek
+ * 
+ * HoleSensor.h
+ * Contains a class that uses Adafruit's MPR121 capacitive touch sensor
+ * library in order to determine what holes are currently pressed on
+ * the ocarina.
+ */
+
+#ifndef HOLESENSOR_H
+#define HOLESENSOR_H
+
+
+
+
+#endif
